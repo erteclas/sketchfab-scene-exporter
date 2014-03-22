b3e26c0a7d67e4c34341970a3858cbca8083987e
reinitialized <reinitialized@users.noreply.github.com>
2014-03-22 00:22:48 -0400

    Add support for new site layout

--- sketchfab-dl-script.user.js
+++ sketchfab-dl-script.user.js
@@ -9,11 +9,23 @@
 // Urls process this user script on
 // @include        /^https?://(www\.)?sketchfab\.com/models/.*$/
 // ==/UserScript==
- 
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, 9, null).singleNodeValue;
-};
 
+var retrievedOBJ = false;
+window.onload=function() {
+    try {
+        window.unsafeWindow.OSG.osgViewer.Viewer.prototype.draw = function() { 
+            if (!retrievedOBJ) {
+                recurse(this.getSceneData());
+            }
+            var a = this.getState();
+            this._renderStage.draw(a), a.popAllStateSets(), a.applyWithoutProgram()
+        };
+    }
+    catch(err) {
+        console.log("failed to override method");
+    }
+}
+ 
 function InfoForGeometry(geom) {
     try {
         info = {
@@ -47,7 +59,8 @@ var numUndefinedObjNames = 0;
 function OBJforGeometryInfo(info) {
     if (!info)
         return;
-    var obj = 'o ' + (info.name ? info.name : ++numUndefinedObjNames) + '\n';
+    var objName = info.name ? info.name : ++numUndefinedObjNames;
+    var obj = 'o ' + objName + '\n';
     for (var i = 0; i < info.vertices.length; i += 3) {
         obj += 'v ';
         for (j = 0; j < 3; ++j) {
@@ -57,11 +70,11 @@ function OBJforGeometryInfo(info) {
     }
     for (var i = 0; i < info.primitives.length; ++i) {
         var primitive = info.primitives[i];
-        if (primitive.mode == gl.TRIANGLES || primitive.mode == gl.TRIANGLE_STRIP) {
-            for (j = 0; j + 2 < primitive.indices.length; primitive.mode == gl.TRIANGLES ? j += 3 : ++j) {
+        if (primitive.mode == 4 || primitive.mode == 5) {
+            for (j = 0; j + 2 < primitive.indices.length; primitive.mode == 4 ? j += 3 : ++j) {
                 obj += 'f ';
                 for (k = 0; k < 3; ++k) {
-                    obj += (primitive.indices[j + k] + vertexOffset + 1) + ' ';
+                    obj += (primitive.indices[j + k] + 1) + ' ';
                 }
                 obj += '\n';
             }
@@ -71,6 +84,10 @@ function OBJforGeometryInfo(info) {
         }
     }
     vertexOffset += info.vertices.length / 3;
+    if (!retrievedOBJ)
+    	alert("Retrieving OBJ, please wait");    
+    retrievedOBJ = true;
+    window.dlOBJ(obj, objName);
     return obj;
 };
 
@@ -97,14 +114,13 @@ function recurse(node) {
         }
     }
 };
-window.dlOBJ = function() {
-    recurse(view3D._scene); 
+window.dlOBJ = function(objString, objName) {
     // Credit: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
     function destroyClickedElement(event)
     {
         document.body.removeChild(event.target);
     }
-    var textToWrite = combinedOBJ;
+    var textToWrite = objString;
     var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
     
     // Credit: http://phpjs.org/functions
@@ -117,7 +133,7 @@ window.dlOBJ = function() {
         
         return b;
     }
-    var fileNameToSaveAs = basename(document.URL) + ".obj";
+    var fileNameToSaveAs = objName + ".obj";
     
     var downloadLink = document.createElement("a");
     downloadLink.download = fileNameToSaveAs;
@@ -139,43 +155,3 @@ window.dlOBJ = function() {
     }
     downloadLink.click();
 };
-// Credit: http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-var observeDOM = (function(){
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-        eventListenerSupported = window.addEventListener;
-
-    return function(obj, callback){
-        if( MutationObserver ){
-            // define a new observer
-            var obs = new MutationObserver(function(mutations, observer){
-                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-                    callback();
-            });
-            // have the observer observe foo for changes in children
-            obs.observe( obj, { childList:true, subtree:true });
-        }
-        else if( eventListenerSupported ){
-            obj.addEventListener('DOMNodeInserted', callback, false);
-            obj.addEventListener('DOMNodeRemoved', callback, false);
-        }
-    }
-})();
-
-var actionsContainerXPath = "/html/body/div[@class='content']/div[@class='view']/div\
-    [@class='container']/div[@class='sections']/div[@class='main']/div[@class='additional']\
-    /div[@class='actions']";
-var buttonAdded = false;
-observeDOM(document.body, function(){ 
-    var actionsContainer = getElementByXpath(actionsContainerXPath);
-    if (actionsContainer && !buttonAdded) {
-        var li=document.createElement("li");
-        li.innerHTML='<a class="button btn-medium btn-secondary"id="downloadOBJ"><span>\
-                      Download .OBJ</span></a>';
-        li.firstChild.addEventListener ("click", dlOBJ , false);
-        actionsContainer.appendChild(li.firstChild);
-        buttonAdded = true;
-    }
-    else if (!actionsContainer && buttonAdded) {
-        buttonAdded = false;   
-    }
-});
