4d003a13ab1da63c690930def402c18175ac0a5d
reinitialized <reinitialized@users.noreply.github.com>
2014-08-09 23:18:13 -0400

    Update sketchfab-dl-script.user.js
    Fix in response to recent website changes. Now outputs separate files.

--- sketchfab-dl-script.user.js
+++ sketchfab-dl-script.user.js
@@ -4,63 +4,56 @@
 // @author         Reinitialized
 //
 //Version Number
-// @version        1.0
+// @version        1.01
 //
 // Urls process this user script on
 // @include        /^https?://(www\.)?sketchfab\.com/models/.*$/
 // ==/UserScript==
 
-var retrievedOBJ = false;
 window.onload=function() {
     try {
-        window.unsafeWindow.OSG.osgViewer.Viewer.prototype.draw = function() { 
-            if (!retrievedOBJ) {
-                recurse(this.getSceneData());
+        window.models = [];
+		OSG.osg.Geometry.prototype.originalDrawImplementation = OSG.osg.Geometry.prototype.drawImplementation;
+		OSG.osg.Geometry.prototype.drawImplementation = function(a) {
+            this.originalDrawImplementation(a);
+            if (!this.computedOBJ) {
+                this.computedOBJ = true;
+                var dlName = document.title + "-" + window.models.length;
+                window.dlOBJ(OBJforGeometry(this), dlName);
             }
-            var a = this.getState();
-            this._renderStage.draw(a), a.popAllStateSets(), a.applyWithoutProgram()
         };
     }
     catch(err) {
         console.log("failed to override method");
     }
-}
+};
  
 function InfoForGeometry(geom) {
-    try {
-        info = {
-            'name' : geom._name,
-            'vertices' : geom.attributes.Vertex._elements,
-            'primitives' : []
-        };
-        for (i = 0; i < geom.primitives.length; ++i) {
-            var primitive = geom.primitives[i];
-            if (primitive.mode == 1)
-                return null;
-            info.primitives.push({
-                'mode' : primitive.mode,
-                'indices' : primitive.indices._elements
-            });
-        }
-        return info;
+    info = {
+        'name' : geom._name,
+        'vertices' : geom.attributes.Vertex._elements,
+        'primitives' : []
+    };
+    for (i = 0; i < geom.primitives.length; ++i) {
+        var primitive = geom.primitives[i];
+        if (primitive.mode == 1)
+            return null;
+        info.primitives.push({
+            'mode' : primitive.mode,
+            'indices' : primitive.indices._elements
+        });
     }
-    catch (err) {
-        console.log(err.message);
-        console.log(geom);
-    }
-};
+    return info;
+}
 
 function OBJforGeometry(geom) {
     return OBJforGeometryInfo(InfoForGeometry(geom));
-};
+}
 
-var vertexOffset = 0;
-var numUndefinedObjNames = 0;
 function OBJforGeometryInfo(info) {
+    var obj = "";
     if (!info)
         return;
-    var objName = info.name ? info.name : ++numUndefinedObjNames;
-    var obj = 'o ' + objName + '\n';
     for (var i = 0; i < info.vertices.length; i += 3) {
         obj += 'v ';
         for (j = 0; j < 3; ++j) {
@@ -83,37 +76,9 @@ function OBJforGeometryInfo(info) {
             console.log('Primitive mode not implemented');
         }
     }
-    vertexOffset += info.vertices.length / 3;
-    if (!retrievedOBJ)
-    	alert("Retrieving OBJ, please wait");    
-    retrievedOBJ = true;
-    window.dlOBJ(obj, objName);
     return obj;
-};
+}
 
-var computedIDs = new Array();
-var combinedOBJ = '';
-function recurse(node) {
-    if (node.className() == 'Geometry') {
-        var computeOBJ = true;
-        var useID = '_uniqueID' in node;
-        for (var i = 0; i < computedIDs.length; ++i) {
-            if (computedIDs[i] == (useID ? node._uniqueID : node._name)) {
-                computeOBJ = false; 
-                break;
-            }
-        }
-        if (computeOBJ) {
-            computedIDs.push(useID ? node._uniqueID : node._name);
-            combinedOBJ += OBJforGeometry(node);
-        }
-    }
-    if (node.children.length) {
-        for (var i = 0; i < node.children.length; ++i) {
-            recurse(node.children[i]);
-        }
-    }
-};
 window.dlOBJ = function(objString, objName) {
     // Credit: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
     function destroyClickedElement(event)
