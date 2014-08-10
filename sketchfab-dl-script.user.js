388c78b13c4411609bd1301fe37f56ded84257e6
reinitialized <reinitialized@users.noreply.github.com>
2014-08-10 10:07:00 -0400

    Get a decent amount of UV-mapping completed

--- sketchfab-dl-script.user.js
+++ sketchfab-dl-script.user.js
@@ -16,30 +16,37 @@ window.onload=function() {
 
 var models = [];
 function overrideDrawImplementation() {
-    models = [];
     OSG.osg.Geometry.prototype.originalDrawImplementation = OSG.osg.Geometry.prototype.drawImplementation;
     OSG.osg.Geometry.prototype.drawImplementation = function(a) {
         this.originalDrawImplementation(a);
         if (!this.computedOBJ) {
             this.computedOBJ = true;
+            this.name = safeName(document.title.replace(' - Sketchfab', '')) + '-' + models.length;
+            this.textures = textureInfoForGeometry(this);
             models.push({
-                name: document.title + "-" + models.length,
-                obj: OBJforGeometry(this)
+                name: this.name,
+                obj: OBJforGeometry(this),
+                mtl: MTLforGeometry(this),
+                textures: this.textures
             });
         }
     };
 }
+
+// source: http://stackoverflow.com/a/8485137
+function safeName(s) {
+    return s.replace(/[^a-zA-Z0-9]/gi, '_').toLowerCase();
+}
  
 function InfoForGeometry(geom) {
-    info = {
-        'name' : geom._name,
+    var info = {
         'vertices' : geom.attributes.Vertex._elements,
-        'primitives' : []
+        'texCoords' : geom.attributes.TexCoord0._elements,
+        'primitives' : [],
+        'name' : geom.name
     };
     for (i = 0; i < geom.primitives.length; ++i) {
         var primitive = geom.primitives[i];
-        if (primitive.mode == 1)
-            return null;
         info.primitives.push({
             'mode' : primitive.mode,
             'indices' : primitive.indices._elements
@@ -48,21 +55,28 @@ function InfoForGeometry(geom) {
     return info;
 }
 
+var nl = '\n';
 function OBJforGeometry(geom) {
-    return OBJforGeometryInfo(InfoForGeometry(geom));
-}
-
-function OBJforGeometryInfo(info) {
-    var obj = "";
-    if (!info)
-        return;
+    var obj = '';
+    var info = InfoForGeometry(geom);
+    obj += 'mtllib ' + MTLFilenameForGeometry(geom) + nl;
+    obj += 'o ' + geom.name + nl;
     for (var i = 0; i < info.vertices.length; i += 3) {
         obj += 'v ';
         for (j = 0; j < 3; ++j) {
             obj += info.vertices[i + j] + ' ';
         }
-        obj += '\n';
+        obj += nl;
     }
+    for (var i = 0; i < info.texCoords.length; i += 2) {
+     	obj += 'vt ';
+        for (j = 0; j < 2; ++j) {
+            obj += info.texCoords[i + j] + ' ';
+        }
+        obj += nl;  
+    }
+    obj += 'usemtl ' + MTLNameForGeometry(geom) + nl;
+    obj += 's on' + nl;
     for (var i = 0; i < info.primitives.length; ++i) {
         var primitive = info.primitives[i];
         if (primitive.mode == 4 || primitive.mode == 5) {
@@ -75,17 +89,84 @@ function OBJforGeometryInfo(info) {
                 for (k = 0; k < 3; ++k) {
                     obj += (primitive.indices[j + order[k]] + 1) + ' ';
                 }
-                obj += '\n';
+                obj += nl;
             }
         }
         else {
-            console.log('Primitive mode not implemented');
+            throw 'Primitive mode not implemented';
         }
     }
     return obj;
 }
 
-// Credit: http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+var textureMTLMap = {
+    DiffuseColor: "map_Kd",
+    SpecularColor: "map_Ks",
+    NormalMap : "map_bump",
+    EmitColor : "map_Ke",
+    AlphaMask : "map_d",
+    Opacity : "map_o"
+};
+
+// source: http://stackoverflow.com/a/3820412
+function baseName(str)
+{
+   var base = new String(str).substring(str.lastIndexOf('/') + 1); 
+    if(base.lastIndexOf(".") != -1)       
+        base = base.substring(0, base.lastIndexOf("."));
+   return base;
+}
+
+function ext(str) {
+    return str.split('.').pop();
+}
+
+function textureInfoForGeometry(geom) {
+    var textureMap = [];
+    if (stateset = geom.stateset) {
+        if (textures = stateset.textureAttributeMapList) {
+            textures.forEach(function(texture) {
+                var object = texture.Texture._object;
+                if (texture = object._texture) {
+                    if (imageProxy = texture._imageProxy) {
+                        var textureURL = imageProxy.attributes.images[0].url;
+                        var texture = {
+                            url: textureURL,
+                            type: textureMTLMap[object._channel._name],
+                            ext: ext(textureURL)
+                        };
+                        texture.filename = textureFilename(geom, texture);
+                        textureMap.push(texture);
+                    }   
+                }
+            });
+        }
+    }
+	return textureMap;
+}
+
+function textureFilename(geom, texture) {
+    return baseName(texture.url) + '.' + texture.ext;
+}
+
+function MTLFilenameForGeometry(geom) {
+ 	return MTLNameForGeometry(geom) + '.mtl';   
+}
+
+function MTLNameForGeometry(geom) {
+ 	return geom.name;
+}
+
+function MTLforGeometry(geom) {
+    var mtl = '';
+    mtl += 'newmtl ' + MTLNameForGeometry(geom) + nl;
+    geom.textures.forEach(function(texture) {
+    	mtl += texture.type + ' ' + texture.filename + nl;
+    });
+    return mtl;
+}
+
+// source: http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
 var observeDOM = (function(){
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
         eventListenerSupported = window.addEventListener;
@@ -108,7 +189,7 @@ var observeDOM = (function(){
 })();
 
 
-// Credit: http://stackoverflow.com/questions/10596417/is-there-a-way-to-get-element-by-xpath-in-javascript
+// source: http://stackoverflow.com/questions/10596417/is-there-a-way-to-get-element-by-xpath-in-javascript
 function getElementByXpath(path) {
     return document.evaluate(path, document, null, 9, null).singleNodeValue;
 }
@@ -124,14 +205,14 @@ observeDOM(document.body, function(){
     }
 });
 
-window.dlOBJ = function(objString, objName) {
-    // Credit: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
+// source: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
+function downloadString(filename, ext, str) {
     function destroyClickedElement(event)
     {
         document.body.removeChild(event.target);
     }
-    var textFileAsBlob = new Blob([objString], {type:'text/plain'});
-    var fileNameToSaveAs = objName + ".obj";
+    var textFileAsBlob = new Blob([str], {type:'text/plain'});
+    var fileNameToSaveAs = filename + '.' + ext;
     
     var downloadLink = document.createElement("a");
     downloadLink.download = fileNameToSaveAs;
@@ -152,15 +233,31 @@ window.dlOBJ = function(objString, objName) {
         document.body.appendChild(downloadLink);
     }
     downloadLink.click();
-};
+}
 
-function dlOBJs() {
+// source: http://muaz-khan.blogspot.com/2012/10/save-files-on-disk-using-javascript-or.html
+function downloadFileAtURL(fileURL) {
+    var save = document.createElement('a');
+    save.href = fileURL;
+    save.target = '_blank';
+    save.download = '';
+    var event = document.createEvent('Event');
+    event.initEvent('click', true, true);
+    save.dispatchEvent(event);
+    (window.URL || window.webkitURL).revokeObjectURL(save.href);
+}
+
+function downloadModels() {
     if (models.length == 0) {
     	alert("Download script failed... try refreshing the page");
         return;
     }
     models.forEach(function(model) {
-        dlOBJ(model.obj, model.name);
+        downloadString(model.name, 'obj', model.obj);
+        downloadString(model.name, 'mtl', model.mtl);
+        model.textures.forEach(function(texture) {
+        	downloadFileAtURL(texture.url);
+        });
     });
 }
 
@@ -168,6 +265,6 @@ function addDownloadButton(downloadButtonParent) {
     var downloadButton = document.createElement("a");
     downloadButton.setAttribute("class", "control");
     downloadButton.innerHTML = "<pre>DOWNLOAD  </pre>";
-    downloadButton.addEventListener("click", dlOBJs , false);
+    downloadButton.addEventListener("click", downloadModels , false);
     downloadButtonParent.appendChild(downloadButton);
 }
